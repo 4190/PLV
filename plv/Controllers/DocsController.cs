de00a77c2915e6cb2cd709ece9ebafefc72c1af8
4190 <lukdab@gmx.es>
2020-04-16 01:34:36 +0200

    Initial upload controller

--- plv/Controllers/DocsController.cs
+++ plv/Controllers/DocsController.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using plv.Models;
@@ -14,9 +15,11 @@ namespace plv.Controllers
     public class DocsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment hostingEnvironment;
 
-        public DocsController(ApplicationDbContext _context)
+        public DocsController(ApplicationDbContext _context, IWebHostEnvironment environment)
         {
+            hostingEnvironment = environment;
             this._context = _context;
         }
         protected override void Dispose(bool disposing)
@@ -28,30 +31,45 @@ namespace plv.Controllers
         {
             return View();
         }
-
+        private string GetUniqueFileName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return Path.GetFileNameWithoutExtension(fileName)
+                      + "_"
+                      + Guid.NewGuid().ToString().Substring(0, 4)
+                      + Path.GetExtension(fileName);
+        }
+        /*
         [HttpPost]
         public IActionResult Create(UploadFileViewModel model)
         {
-            //Create an object of your entity class and map property values
-            var post = new UploadFile{  Name = model.Name };
+            
+            if(model.File != null)
+            {
+                var uniqueFileName = GetUniqueFileName(model.File.FileName);
+                var uploads = Path.Combine(hostingEnvironment.WebRootPath, "uploads");
+                var filePath = Path.Combine(uploads, uniqueFileName);
+                model.File.CopyTo(new FileStream(filePath, FileMode.Create));
+            }
+            return Content("ok");
+        }
+        */
 
+        [HttpPost]  //TODO
+        public IActionResult Create(UploadFileViewModel model)
+        {
             if (model.File != null)
             {
-                post.FileBinary = GetByteArrayFromFile(model.File);
-            }
-            _context.Files.Add(post);
-            _context.SaveChanges();
-            return RedirectToAction("Create", "Docs");
+                string ImageName = Guid.NewGuid().ToString() + Path.GetExtension(model.File.FileName);
 
-        }
+                string SavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", ImageName);
 
-        private byte[] GetByteArrayFromFile(IFormFile file)
-        {
-            using (var target = new MemoryStream())
-            {
-                file.CopyTo(target);
-                return target.ToArray();
+                using (var stream = new FileStream(SavePath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
             }
+            return Content("k");
         }
     }
 }
