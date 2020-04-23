5d580ae416670dbf27d678369c7baa88ebe78a5a
4190 <lukdab@gmx.es>
2020-04-23 18:54:09 +0200

    part of ManageUser for admin

--- plv/Controllers/AdminController.cs
+++ plv/Controllers/AdminController.cs
@@ -7,16 +7,23 @@ using plv.Data;
 using plv.Models;
 using plv.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace plv.Controllers
 {
     [Authorize(Roles ="Admin")]
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApplicationDbContext _context;
 
-        public AdminController(ApplicationDbContext _context)
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager, ApplicationDbContext _context)
         {
+            _roleManager = roleManager;
+            _userManager = userManager;
             this._context = _context;
         }
         protected override void Dispose(bool disposing)
@@ -31,5 +38,38 @@ namespace plv.Controllers
             return View(users);
         }
 
+        [Route("admin/ManageUser/{id}")]
+        public IActionResult ManageUser(string id)
+        {
+            ManageUserViewModel user = new ManageUserViewModel
+            {
+                User = _userManager.FindByIdAsync(id).Result,
+                Role = _roleManager.Roles.ToList()
+            };        
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ManageUserViewModel user)
+        {
+            return Content(user.User.UserName);
+            /*
+            if(!ModelState.IsValid)
+            {
+                ManageUserViewModel model = user;
+                return View("ManageUser", user);
+            }
+
+            if (user.User.Id == "" || user.User.Id == null) { return NotFound(); }
+
+            else
+            {
+                _userManager.AddToRoleAsync(user.User, user.SelectedRole.Name);
+            }
+            return RedirectToAction("Admin", "UserList");
+            */
+        }
+        
     }
 }
