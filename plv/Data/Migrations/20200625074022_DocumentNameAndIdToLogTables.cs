8c5e746e1e9fe77642b37853c6bb0e3bd7c8e1d0
4190 <lukdab@gmx.es>
2020-06-25 09:42:05 +0200

    added docName and docId fields to logging tables

---
+++ plv/Data/Migrations/20200625074022_DocumentNameAndIdToLogTables.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace plv.Data.Migrations
+{
+    public partial class DocumentNameAndIdToLogTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentId",
+                table: "Downloads",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentName",
+                table: "Downloads",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DocumentId",
+                table: "DocumentEdits",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DocumentName",
+                table: "DocumentEdits",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DocumentId",
+                table: "Downloads");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentName",
+                table: "Downloads");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentId",
+                table: "DocumentEdits");
+
+            migrationBuilder.DropColumn(
+                name: "DocumentName",
+                table: "DocumentEdits");
+        }
+    }
+}
--- plv/Models/DocEdits.cs
+++ plv/Models/DocEdits.cs
@@ -8,8 +8,11 @@ namespace plv.Models
     public class DocEdits
     {
         public int Id { get; set; }
+        public string DocumentName { get; set; }
+        public int DocumentId { get; set; }
         public string EditedBy { get; set; }
         public DateTime EditTime { get; set; }
+
 
         public DateTime NewDateIssued { get; set; }
         public string PreviousUser { get; set; }
